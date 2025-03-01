537de8a762f5992307c051aa4f05e8427e5c3cf1
Remi Gau <remi_gau@hotmail.com>
2025-03-01 22:29:02 +0100

    add podcasts as metadata

--- maint_tools/opml.py
+++ maint_tools/opml.py
@@ -1,47 +1,42 @@
-import listparser
+"""Convert OPML file to YAML file.
+
+TODO:
+- check if podcasts still exist
+"""
+import xml.etree.ElementTree as ET
 from pathlib import Path
+import yaml
 
 root_folder = Path(__file__).parents[1]
 
 opml_file = root_folder / "OPML_export_20250301_202833.opml"
 
-result = listparser.parse(opml_file.open().read())
+output_yaml = root_folder / "_data" / "podcasts.yml"
+
+def parse_opml(file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    
+    podcasts = []
+    
+    for outline in root.findall(".//outline"):
+        podcast = {
+            "title": outline.get("text"),
+            "type": outline.get("type"),
+            "rss_url": outline.get("xmlUrl"),
+            "html_url": outline.get("htmlUrl"),
+            "image_url": outline.get("imageUrl")
+        }
+        if podcast["title"] != "Comments on":
+            podcasts.append(podcast)
+    
+    return podcasts
+
+def save_to_yaml(data, output_file):
+    with open(output_file, "w", encoding="utf-8") as file:
+        yaml.dump(data, file, default_flow_style=False, allow_unicode=True)
 
 
-content = """---
-layout: default
-title: Podcasts roll
-permalink: /podcasts.html
----
+podcasts = parse_opml(opml_file)
+save_to_yaml(podcasts, output_yaml)
 
-<div class="row">
-    <div class="col-12 text-center">
-        <div class="navy-line"></div>
-        <h1>Podcasts I listen to.</h1>
-    </div>
-
-    <div class="post-content">
-
-    <div class="col-12 text-center">
-        <p>I use <a href="https://podcastaddict.com/">PodcastAddict</a> to manage my subscriptions.</p>
-    </div>
-
-    <div class="row justify-content-center">
-    <div class="col-8">
-        <ul>
-"""
-
-for x in result.feeds:
-    content += f"            <li> <a href='{x.url}'>{x['title']}</a> </li>\n"
-
-content += """
-        </ul>
-    </div>
-    </div>
-    </div>
-</div>
-"""
-
-oupout_file = root_folder / "podcasts.html"
-with oupout_file.open("w") as f:
-    f.write(content)
