879c7f191ac7f913e65ba425722f56329bf7c045
Remi Gau <remi_gau@hotmail.com>
2025-03-16 22:03:40 +0100

    fix carousel

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,11 +1,13 @@
-// let myCarouselElement = document.querySelector('#myCarousel')
-
-// let carousel = new bootstrap.Carousel('myCarousel')
-
-
 const myCarouselElement = document.querySelector('#myCarousel')
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
+
+const myCarouselElement2 = document.querySelector('#myCarousel2')
+
+const carousel2 = new bootstrap.Carousel(myCarouselElement2, {
   interval: 2000,
   touch: false
 })
