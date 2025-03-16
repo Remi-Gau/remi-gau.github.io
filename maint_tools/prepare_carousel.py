575a5a9069f713f94f37ec0a65fe8a6da0e02fa3
Remi Gau <remi_gau@hotmail.com>
2025-03-16 23:09:48 +0100

    add image

--- maint_tools/prepare_carousel.py
+++ maint_tools/prepare_carousel.py
@@ -7,6 +7,14 @@ output = [
     {"src": f"carousel/{image.name}", "title": image.stem}
     for image in (static_folder / "assets" / "img" / "carousel").glob("*.jpg")
 ]
+output.extend(
+    [
+        {"src": f"carousel/{image.name}", "title": image.stem}
+        for image in (static_folder / "assets" / "img" / "carousel").glob(
+            "*.png"
+        )
+    ]
+)
 
 
 with (root_folder / "_data" / "index" / "banners.yml").open("w") as f:
