0d59f555ffab061d457c220a37fdce0142e6d494
Remi Gau <remi_gau@hotmail.com>
2026-01-11 09:22:07 +0100

    Update maint_tools/convert_recipes.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- maint_tools/convert_recipes.py
+++ maint_tools/convert_recipes.py
@@ -177,7 +177,7 @@ def _compute_calories(ingredient) -> int | float:
             * ingredients_json_content[ingredient_key]["calories"]
         )
     else:
-        print(f"[red]\tunknonw unit for {name}: {unit}")
+        print(f"\tunknown unit for {name}: {unit}")
         cal = 0
 
     return cal
